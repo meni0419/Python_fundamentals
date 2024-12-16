ef19d439b2a26d6f23670da175b7f18e13934dc0
meni0419 <meni0419@gmail.com>
2024-12-16 11:31:54 +0200

    Added practice

---
+++ pracrice.py
@@ -0,0 +1,19 @@
+# # pr1
+# a = 4
+# b = 9
+#
+# print(f"{a}, {b}")
+
+# pr2
+name = input("Enter your name: ")
+
+print("Hello, ", name)  # print my name
+# print(f"Hello, {name}")  # print my name
+
+# # pr3
+# print("Python is very interesting to learn")
+#
+# # pr4
+# print(f"Robert: \n"
+#       f"Stannis \n"
+#       f"Renly \n")
