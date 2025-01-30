f7081c9eef601034c9e39b0767e0f49b11f6f749
meni0419 <meni0419@gmail.com>
2025-01-30 10:10:34 +0200

    changes from lesson

--- ALL_HOMEWORK/homework_8/practice_py_14.py
+++ ALL_HOMEWORK/homework_8/practice_py_14.py
@@ -4,7 +4,7 @@ from decimal import Decimal, getcontext
 getcontext().prec = 28
 
 # Function to calculate the sum of the Leibniz series
-def sum_of_leibniz(n):
+def sum_of_leibniz(n=1000):
     i = Decimal('3')  # Denominator starts at 3
     s_sum = Decimal('1')  # Initial sum starts with 1
     plus = False  # Toggle for addition and subtraction
@@ -22,14 +22,14 @@ def sum_of_leibniz(n):
 
     return s_sum  # Return the calculated sum
 
-
+print(sum_of_leibniz())
 # Prompt the user for input and validate it
-try:
-    num = int(input("Enter a positive integer for the number of terms: "))
-    if num <= 0:
-        raise ValueError("The number must be a positive integer.")
-    # Print the result of the calculated Leibniz sum
-    print(f"Sum of first {num} terms of the Leibniz series: {sum_of_leibniz(num)}")
-except ValueError as e:
-    # Handle cases where the input is invalid
-    print(f"Invalid input: {e}")
+# try:
+#     num = int(input("Enter a positive integer for the number of terms: "))
+#     if num <= 0:
+#         raise ValueError("The number must be a positive integer.")
+#     # Print the result of the calculated Leibniz sum
+#     print(f"Sum of first {num} terms of the Leibniz series: {sum_of_leibniz(num)}")
+# except ValueError as e:
+#     # Handle cases where the input is invalid
+#     print(f"Invalid input: {e}")
