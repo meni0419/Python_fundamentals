e8b1ac354660820c3a8a04982f1b308fae3cc8c8
meni0419 <meni0419@gmail.com>
2025-02-08 15:16:57 +0200

    added hw 19

--- ALL_HOMEWORK/homework_19/hw_19.py
+++ ALL_HOMEWORK/homework_19/hw_19.py
@@ -9,20 +9,46 @@
 Пример вывода:
 Анаграммы: ['dog', 'god'], ['cat', 'tac', 'act']"""
 
-def anagrams(words):
-    anagram_list = []
-    word_set = ''
+
+def anagrams_v1(words):
+    words_copy = words.copy()
+    anagrams_str = []
     for word in words:
-        for letter in word:
-            word_set += ''
-        anagram_list.append(word_set)
+        anagram_list = []
+        for word2 in words_copy:
+            if set(word) == set(word2):
+                anagram_list.append(word2)
+                words.remove(word2)
+        anagrams_str.append(anagram_list)
+    return anagrams_str
 
 
 words = ['cat', 'dog', 'tac', 'god', 'act']
-words_set = set(words)
-anagrams(words)
+print(f"Анаграммы (v1): {', '.join(str(group) for group in anagrams_v1(words) if len(group) > 1)}")
 
 
+def anagrams_v2(words):
+    # Создаем словарь для группировки анаграмм
+    anagram_dict = {}
+
+    # Проходим по каждому слову в списке
+    for word in words:
+        # Сортируем буквы в слове и используем их как ключ
+        sorted_word = ''.join(sorted(word))
+
+        # Если ключ уже есть в словаре, добавляем слово в список анаграмм
+        if sorted_word in anagram_dict:
+            anagram_dict[sorted_word].append(word)
+        # Иначе создаем новую запись в словаре
+        else:
+            anagram_dict[sorted_word] = [word]
+
+    # Возвращаем только те списки, где больше одного слова (анаграммы)
+    return [group for group in anagram_dict.values() if len(group) > 1]
+
+
+words = ['cat', 'dog', 'tac', 'god', 'act']
+print("Анаграммы (v2):", ', '.join(str(group) for group in anagrams_v2(words)))
 
 """
 Напишите функцию is_subset, которая принимает два множества set1 и set2 и проверяет, является ли set1 подмножеством set2. 
@@ -34,3 +60,13 @@ anagrams(words)
 {1, 2, 3, 4, 5}
 Пример вывода:
 True"""
+
+def is_subset(set1, set2):
+    for item in set1:
+        if item in set2:
+            return True
+        else:
+            return False
+set_1 = {1, 2, 3}
+set_2 = {1, 2, 3, 4, 5}
+print(f"Является ли {set_1} подмножеством {set_2}? {is_subset(set_1, set_2)}")
