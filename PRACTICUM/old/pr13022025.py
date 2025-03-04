25a5251f1f3aea3cbd4cea6ed0d6283a792514bc
meni0419 <meni0419@gmail.com>
2025-03-04 14:53:44 +0200

    interesting task on recursion

--- PRACTICUM/old/pr13022025.py
+++ PRACTICUM/old/pr13022025.py
@@ -1,5 +1,30 @@
-def multiply(num, con=2):
-    res = num * con
-    return res
+# хотите задачку на рекурсию? Одна из тех, что мне вчера чат GPT дал.
+# Дан список, элементами которого могут быть числа и списки с бесконечной вложенностью.
+# Вернуть сумму всех чисел   этого списка. Через рекурсию.
+# Например [1, 2, 3, [1, 1, [1, 2],1], 1, 1]
+# должно вернуть 14
 
-print(multiply(2, 3))
+def sum_list(lst, summ=0):
+    if not lst:
+        return summ
+    return sum_list(lst[1:],
+                    summ + (lst[0] if isinstance(lst[0], int) else sum(
+                        [i if isinstance(i, int) else sum(i) for i in lst[0]])))
+
+
+print(sum_list([1, 2, 3, [1, 1, [1, 2], 1], 1, 1]))
+
+
+# lst = [[1, 1, [1, 2],1], 1, 1]
+# a = [i if type(i) == int else sum(i) for i in lst[0]]
+# print(sum(a))
+
+
+def sum_list_GPT(lst, summ=0):
+    if not lst:
+        return summ
+    return sum_list_GPT(lst[1:],
+                        summ + (lst[0] if isinstance(lst[0], int) else sum_list_GPT(lst[0], 0)))
+
+
+print(sum_list_GPT([1, 2, 3, [1, 1, [1, 2], 1], 1, 1]))
