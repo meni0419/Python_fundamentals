e3a2d57d1183e82a718d66f37d1babe0d90cd72c
Max <meni0419@gmail.com>
2025-03-05 05:31:41 +0200

    hw 20 added solution for 20_2

--- ALL_HOMEWORK/homework_20/hw_20_2.py
+++ ALL_HOMEWORK/homework_20/hw_20_2.py
@@ -12,4 +12,33 @@ def count_unique_chars(u_str: str) -> int:
     return len(set(u_str.lower()))
 
 text = input("Введите строку: ")
-print("Количество уникальных символов:", count_unique_chars(text))
+print("Количество уникальных символов (set):", count_unique_chars(text))
+
+"""СО СЛОВАРЕМ"""
+
+def count_unique_chars_2(u_str: str) -> int:
+    chars_count = {}
+    for char in u_str.lower():
+        if char in chars_count:
+            chars_count[char] += 1
+        else:
+            chars_count[char] = 1
+    return len(chars_count)
+
+print("Количество уникальных символов (dict):", count_unique_chars_2(text))
+
+"""GPT учит крутым штукам"""
+
+def count_unique_chars_3(u_str: str) -> int:
+    return len({char: u_str.lower().count(char) for char in u_str.lower()})
+
+# Пример использования
+print("Количество уникальных символов:(comprehension)", count_unique_chars_3(text))
+
+from collections import Counter
+
+def count_unique_chars_4(u_str: str) -> int:
+    return len(Counter(u_str.lower()))
+
+# Пример использования
+print("Количество уникальных символов (Counter):", count_unique_chars_4(text))
