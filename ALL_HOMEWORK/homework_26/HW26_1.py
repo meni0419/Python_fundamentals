2fce272ce4d6dd6ed84f0c5f7697bf6d735a55b4
meni0419 <meni0419@gmail.com>
2025-03-28 10:52:58 +0200

    HW26_1 added default and def

--- ALL_HOMEWORK/homework_26/HW26_1.py
+++ ALL_HOMEWORK/homework_26/HW26_1.py
@@ -1,13 +1,18 @@
 """1. Напишите программу, которая принимает в качестве аргумента командной строки путь к файлу .py и запускает его. При запуске файла программа должна выводить сообщение "Файл <имя файла> успешно запущен". Если файл не существует или не может быть запущен, программа должна вывести соответствующее сообщение об ошибке."""
 
-import sys
 import os
 import argparse
 
+
+def run_script(script_path):
+    if os.path.exists(script_path):
+        os.system(f"python {script_path}")
+        print(f"File {script_path} successfully launched")
+    else:
+        print(f"File {script_path} does not exist")
+
+
 parser = argparse.ArgumentParser(description="Run a python script")
-parser.add_argument("script", help="enter path to the script")
+parser.add_argument("script", help="enter path to the script", default="./HW26_2.py", nargs="?")
 args = parser.parse_args()
-if os.path.exists(args.script):
-    os.system(f"python {args.script}")
-else:
-    print(f"File {args.script} does not exist")
+run_script(args.script)
