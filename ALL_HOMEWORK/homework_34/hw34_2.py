22c926f03d4ab42c3a8eb302580cfa5894be7174
meni0419 <meni0419@gmail.com>
2025-05-05 15:02:31 +0300

    HW34

--- ALL_HOMEWORK/homework_34/hw34_2.py
+++ ALL_HOMEWORK/homework_34/hw34_2.py
@@ -2,11 +2,14 @@
 Напишите функцию highlight_keywords(text, keywords), которая выделяет все вхождения заданных ключевых слов в тексте, окружая их символами *. Функция должна быть регистронезависимой при поиске ключевых слов.
 
 Пример использования:"""
+import re
 
-def highlight_keywords(text, keywords):
-    for keyword in keywords:
-        text = text.replace(keyword, f"*{keyword}*")
-    return text
+
+def highlight_keywords(text_, keywords_):
+    for keyword in keywords_:
+        text_ = re.sub(rf'\b{keyword}\b', lambda match: f'*{match.group()}*', text_, flags=re.IGNORECASE)
+    return text_
+
 
 text = "This is a sample text. We need to highlight Python and programming."
 
